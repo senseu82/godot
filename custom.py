1221dd4b7414a12abd9fcbece8a86cfc12223f8f
senseu82 <senseu82@gmail.com>
2026-06-18 04:34:08 +0300

    custom.py

---
+++ custom.py
@@ -0,0 +1,21 @@
+# Оптимизация для чистой 2D игры (Godot 4.6+)
+optimize = "size"
+disable_3d = "yes"
+disable_advanced_gui = "yes"  # отключает сложные текстовые редакторы внутри движка
+
+# Отключение ненужных модулей
+module_arkit_enabled = "no"
+module_camera_enabled = "no"
+module_gltf_enabled = "no"
+module_godot_physics_3d_enabled = "no"
+module_interpenetration_enabled = "no"
+module_jsonrpc_enabled = "no"
+module_minimp3_enabled = "no"  # если используете только .ogg/.wav для звука
+module_msdfgen_enabled = "no"
+module_openxr_enabled = "no"
+module_raycast_enabled = "no"
+module_text_server_fb_enabled = "no"
+module_upnp_enabled = "no"
+module_vhacd_enabled = "no"
+module_vorbis_enabled = "no"
+module_webxr_enabled = "no"
